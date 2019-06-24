f023988eea29e260861cb304bb9c1705c1c97868
marcel <m.tilly@hotmail.com>
2019-06-24 22:17:42 +0200

    Gradle fix

--- src/test/java/uebung11/CustomerTest.java
+++ src/test/java/uebung11/CustomerTest.java
@@ -1,9 +1,9 @@
 package uebung11;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
 
@@ -13,7 +13,7 @@ public class CustomerTest {
 
     private Customer customer;
 
-    @Before
+    @BeforeEach
     public void initCustomer() {
         customer = new Customer("fred");
 
@@ -25,37 +25,37 @@ public class CustomerTest {
     @Test
     public void basicChildrenRental() {
         customer.addRental(new Rental(THE_HULK, 2));
-        Assert.assertEquals(customer.statement(), expectedMessageFor("The Hulk", 1.5, 1.5, 1));
+        assertEquals(customer.statement(), expectedMessageFor("The Hulk", 1.5, 1.5, 1));
     }
 
     @Test
     public void shouldDiscountChildrensRentals() {
         customer.addRental(new Rental(THE_HULK, 4));
-        Assert.assertEquals(customer.statement(), expectedMessageFor("The Hulk", 3.0, 3.0, 1));
+        assertEquals(customer.statement(), expectedMessageFor("The Hulk", 3.0, 3.0, 1));
     }
 
     @Test
     public void basicNewReleaseRental() {
         customer.addRental(new Rental(IRON_MAN, 1));
-        Assert.assertEquals(customer.statement(), expectedMessageFor("Iron Man 4", 3.0, 3.0, 1));
+        assertEquals(customer.statement(), expectedMessageFor("Iron Man 4", 3.0, 3.0, 1));
     }
 
     @Test
     public void shouldNotDiscountNewReleaseRentalsButBonusFrequentRenterPoints() {
         customer.addRental(new Rental(IRON_MAN, 4));
-        Assert.assertEquals(customer.statement(), expectedMessageFor("Iron Man 4", 12.0, 12.0, 2));
+        assertEquals(customer.statement(), expectedMessageFor("Iron Man 4", 12.0, 12.0, 2));
     }
 
     @Test
     public void basicRegularRental() {
         customer.addRental(new Rental(SPIDER_MAN, 2));
-        Assert.assertEquals(customer.statement(), expectedMessageFor("Spiderman", 2.0, 2.0, 1));
+        assertEquals(customer.statement(), expectedMessageFor("Spiderman", 2.0, 2.0, 1));
     }
 
     @Test
     public void shouldDiscountRegularRental() {
         customer.addRental(new Rental(SPIDER_MAN, 4));
-        Assert.assertEquals(customer.statement(), expectedMessageFor("Spiderman", 5.0, 5.0, 1));
+        assertEquals(customer.statement(), expectedMessageFor("Spiderman", 5.0, 5.0, 1));
     }
 
     @Test
@@ -63,7 +63,7 @@ public class CustomerTest {
         customer.addRental(new Rental(THE_HULK, 2));
         customer.addRental(new Rental(SPIDER_MAN, 1));
         customer.addRental(new Rental(IRON_MAN, 3));
-        Assert.assertEquals(customer.statement(), "Rental record for fred\n\tThe Hulk\t1.5\n\tSpiderman\t2.0\n\tIron Man 4\t9.0\nAmount owed is 12.5\nYou earned 4 frequent renter points");
+        assertEquals(customer.statement(), "Rental record for fred\n\tThe Hulk\t1.5\n\tSpiderman\t2.0\n\tIron Man 4\t9.0\nAmount owed is 12.5\nYou earned 4 frequent renter points");
     }
 
     private static String expectedMessageFor(String rental, double price, double total, int renterPointsEarned) {
